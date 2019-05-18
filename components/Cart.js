105a4aea6cadc890de81a1208136760d65a625d8
Danny Vu <danny.hvu@gmail.com>
2019-05-18 21:42:04 +0200

    Implement Stripe payment at checkout to get the token

--- components/Cart.js
+++ components/Cart.js
@@ -10,6 +10,7 @@ import SickButton from './styles/SickButton';
 import CartItem from './CartItem';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import formatMoney from '../lib/formatMoney';
+import Checkout from './Checkout';
 
 const LOCAL_STATE_QUERY = gql`
   query {
@@ -55,7 +56,9 @@ const Cart = () => (
           </ul>
           <footer>
             <p>{formatMoney(calcTotalPrice(me.cart))}</p>
-            <SickButton>Checkout</SickButton>
+            <Checkout>
+              <SickButton>Checkout</SickButton>
+            </Checkout>
           </footer>
         </CartStyles>
       );
---
+++ components/Checkout.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+import { Mutation } from 'react-apollo';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+import PropTypes from 'prop-types';
+import gql from 'graphql-tag';
+import calcTotalPrice from '../lib/calcTotalPrice';
+import Error from './ErrorMessage';
+import User, { CURRENT_USER_QUERY } from './User';
+
+const totalItems = cart => {
+  return cart.reduce((tally, cartItem) => tally + cartItem.quantity, 0);
+};
+
+class Checkout extends React.Component {
+  onToken = res => {
+    console.log(res);
+  };
+
+  render() {
+    return (
+      <User>
+        {({ data: { me } }) => (
+          <StripeCheckout
+            amount={calcTotalPrice(me.cart)}
+            name="Danny's Store"
+            description={`Order of ${totalItems(me.cart)} items`}
+            image={me.cart[0].item && me.cart[0].item.image}
+            stripeKey="pk_test_htp41JjvrYB46NnOcOOsmZAt00OVcGM8p9"
+            currency="USD"
+            email={me.email}
+            token={res => this.onToken(res)}
+          >
+            {this.props.children}
+          </StripeCheckout>
+        )}
+      </User>
+    );
+  }
+}
+
+export default Checkout;
