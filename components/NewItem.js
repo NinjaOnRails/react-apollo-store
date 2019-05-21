a5347fb79bf78b88291aed26ce4f6e183b2fe1a0
Danny Vu <danny.hvu@gmail.com>
2019-05-21 12:50:38 +0200

    Add refetch items and pagination queries after new item creation

--- components/NewItem.js
+++ components/NewItem.js
@@ -4,6 +4,8 @@ import gql from 'graphql-tag';
 import Router from 'next/router';
 import Form from './styles/Form';
 import Error from './ErrorMessage';
+import { ALL_ITEMS_QUERY } from './Items';
+import { PAGINATION_QUERY } from './Pagination';
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -65,7 +67,14 @@ class NewItem extends Component {
     const { title, price, description, image } = this.state;
 
     return (
-      <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
+      <Mutation
+        mutation={CREATE_ITEM_MUTATION}
+        variables={this.state}
+        refetchQueries={[
+          { query: ALL_ITEMS_QUERY },
+          { query: PAGINATION_QUERY },
+        ]}
+      >
         {(createItem, { loading, error }) => (
           <Form
             data-test="form"
